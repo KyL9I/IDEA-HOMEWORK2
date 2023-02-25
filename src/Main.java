3f78c7c2a9d8d1981c7e5f8329a8966b2ea3a401
Alex test <alexkulikov09@mail.ru>
2023-02-25 22:24:55 +0300

    dorabotka

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Main {
     public static void main(String[] args) {
-         boolean isRegistered = true;
-         int price = 10567;
+        int price = 10567;
+        int mile = 20;
 
-         int mile;
-         if (isRegistered) {
-             mile = 20;
-         } else {
-             mile = 0;
-         }
-       int bonus = price / mile;
-         System.out.println(bonus);
-         
+        int bonus = price / mile;
+        System.out.println(bonus);
     }
+
+
 }
